764a06fa4c01802dc3abc160bf6899070cfe3a89
Martin Schneppenheim <weeco91@gmail.com>
2020-08-25 14:31:15 +0200

    Remove provider config from module

---
+++ modules/kowl/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    google     = ">= 3.36.0, <4.0.0"
+    kubernetes = ">= 1.12.0, <2.0.0"
+    random     = ">= 2.3.0, <3.0.0"
+  }
+}
