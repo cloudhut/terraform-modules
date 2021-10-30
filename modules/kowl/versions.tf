c32e68e7d91afbeb887fd2ec224175aa1dbc6478
Martin Schneppenheim <weeco91@gmail.com>
2021-10-30 12:38:15 +0200

    Fix issue #5
    Remove Google provider from required block

--- modules/kowl/versions.tf
+++ modules/kowl/versions.tf
@@ -2,7 +2,6 @@ terraform {
   required_version = ">=0.12"
 
   required_providers {
-    google     = ">= 3.36.0, <4.0.0"
     kubernetes = ">= 2.0.0, <3.0.0"
     random     = ">= 2.3.0, <3.0.0"
   }
