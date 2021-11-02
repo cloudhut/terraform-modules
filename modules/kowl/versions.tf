434c37753c5195478ff1a4c198aadf1a1ca2be1b
kayibal <alan@datarevenue.com>
2021-11-02 19:35:34 +0100

    remove google provider, less restrictive random

--- modules/kowl/versions.tf
+++ modules/kowl/versions.tf
@@ -2,8 +2,7 @@ terraform {
   required_version = ">=0.12"
 
   required_providers {
-    google     = ">= 3.36.0, <4.0.0"
     kubernetes = ">= 2.0.0, <3.0.0"
-    random     = ">= 2.3.0, <3.0.0"
+    random     = ">= 2.3.0, <=3.1.0"
   }
 }
